3e2edad9a8bad7f07985a27742252208b923cea6
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-07-18 12:15:34 -0700

    js functionality update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const apiKey = "fc598b6a4d65a95e4d481065c9123c43";
+  const apiKey = "YOUR_API_KEY";
 
   function fetchWeather(city) {
     const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
@@ -15,8 +15,10 @@ $(document).ready(function () {
 
   function displayCurrentWeather(data) {
     const currentWeather = data.list[0];
+    const weatherIcon = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
     const weatherHTML = `
             <h2>${data.city.name} (${dayjs().format("M/D/YYYY")})</h2>
+            <img src="${weatherIcon}" alt="Weather icon">
             <p>Temp: ${currentWeather.main.temp} °F</p>
             <p>Wind: ${currentWeather.wind.speed} MPH</p>
             <p>Humidity: ${currentWeather.main.humidity} %</p>
@@ -28,9 +30,11 @@ $(document).ready(function () {
     let forecastHTML = "<h2>5-Day Forecast:</h2>";
     for (let i = 1; i < data.list.length; i += 8) {
       const day = data.list[i];
+      const weatherIcon = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
       forecastHTML += `
                 <div>
                     <h3>${dayjs(day.dt_txt).format("M/D/YYYY")}</h3>
+                    <img src="${weatherIcon}" alt="Weather icon">
                     <p>Temp: ${day.main.temp} °F</p>
                     <p>Wind: ${day.wind.speed} MPH</p>
                     <p>Humidity: ${day.main.humidity} %</p>
